66ad26db608aed4ff11c6abec7bcbe611610c215
ChrisHuddart <chrishuddart@gmail.com>
2019-05-12 16:38:24 +0100

    This should be it - done as per the tutorial

--- app/src/main/java/com/example/quicklauncher/MainActivity.java
+++ app/src/main/java/com/example/quicklauncher/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.quicklauncher;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -32,17 +33,26 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // establish a reference to the on-screen secondActivityButton button
-        Button secondActivityButton = (Button) findViewById(R.id.secondActivityButton);
+        Button googleSearchButton = (Button) findViewById(R.id.googleSearchButton);
         // set an 'on click' listener activity
-        secondActivityButton.setOnClickListener(new View.OnClickListener() {
+        googleSearchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent startIntent = new Intent (getApplicationContext(), SecondActivity.class);
+                // define variables
 
-                //Add parameters to be passed to the SecondActivity class that is called when the
-                //secondActivityButton on the MainActivity screen (MainActivity.View ??)
-                startIntent.putExtra("com.example.quicklauncher.FIRSTNAME", "First value");
-                startActivity(startIntent);
+                // setup URL to be sent to the browser (or parameters for any other external app)
+                // String tempGoogleURL = "https://google.com";
+                // a URI (Universal Resource Identifier is used to send parameters to external app??
+                // x = String "https://google.com";
+                Uri googleURI = Uri.parse((String) "https://google.com");
+
+                // now setup the intent - the action that the user intends to do, in this case
+                // launch a browser and open it at google.com
+                Intent launchGoogle = new Intent(Intent.ACTION_VIEW, googleURI);
+                // check that there is a package
+                if (launchGoogle.resolveActivity(getPackageManager()) != null){
+                    startActivity(launchGoogle);
+                }
             }
         });
 
